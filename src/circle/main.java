0dac84f2b3c866e5abf5e482a509407258351af4
erikte15c <erik.danielsson.te15d@gmail.com>
2017-11-08 13:53:39 +0100

    Skapade ett tomt fönster

---
+++ src/circle/main.java
@@ -0,0 +1,16 @@
+package circle;
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+public class main extends Application{
+	public void start(Stage stage)throws Exception{
+		Group root = new Group();
+		Scene scene = new Scene(root, 500, 500);
+		stage.setScene(scene);
+		stage.show();
+	}
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
