da67849e1cd51024f31f61df420a99d03953286b
erikte15c <erik.danielsson.te15d@gmail.com>
2017-11-08 13:59:36 +0100

    Skapade en cirkel i mitten av rutan

--- src/circle/main.java
+++ src/circle/main.java
@@ -2,11 +2,19 @@ package circle;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 public class main extends Application{
 	public void start(Stage stage)throws Exception{
 		Group root = new Group();
 		Scene scene = new Scene(root, 500, 500);
+		Circle cir = new Circle();
+		cir.setRadius(100);
+		cir.setFill(Color.BLACK);
+		cir.setTranslateX(250);
+		cir.setTranslateY(250);
+		root.getChildren().addAll(cir);
 		stage.setScene(scene);
 		stage.show();
 	}
